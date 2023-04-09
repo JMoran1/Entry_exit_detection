d26b17a577f02c9f81ac63c8c933554a21f0c10e
JMoran1 <45739562+JMoran1@users.noreply.github.com>
2023-04-09 21:06:53 +0100

    Added contact views

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ from sqlalchemy_utils import PasswordType
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.sqlite3'
-login_manager = LoginManager()
-login_manager.init_app(app)
+# login_manager = LoginManager()
+# login_manager.init_app(app)
 db = SQLAlchemy(app)
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(PasswordType(schemes=['pbkdf2_sha512']))
+    password = db.Column(db.String(255), nullable=False)
 
 
 class ContactDetail(db.Model):
@@ -50,6 +50,10 @@ def create_tables():
     # db.session.add(test_user)
     # db.session.commit()
 
+    # test_contact = ContactDetail(name='test', email='test', phone='test', user_id=1)
+    # db.session.add(test_contact)
+    # db.session.commit()
+
 
 @app.route('/')
 def home():
@@ -64,23 +68,13 @@ def login():
         password = request.form['password']
 
         user = User.query.filter_by(username=username).first()
-        
-        # if user and user.password.hash(bytes(password, 'utf-8')):
-
-        #     session['user_id'] = user.id
-        #     return redirect(url_for('home'))
-
-        # flash('Invalid username or password')
-        # return redirect(url_for('login'))
 
         if user:
                 print(user.password)
             # Hash the user-entered password and compare to the stored hash
-                hashed_password = user.password.hash(password)
-                print(hashed_password)
-                # if hashed_password == user.password:
-                    # session['user_id'] = user.id
-                return redirect(url_for('home'))
+                if password == user.password:
+                    session['user_id'] = user.id
+                    return redirect(url_for('home'))
 
     return render_template('login.html')
 
@@ -89,6 +83,54 @@ def login():
 def logout():
     session.pop('user_id', None)
     return redirect(url_for('login'))
+
+class ContactsView:
+    def view_contacts(self):
+        contacts = ContactDetail.query.all()
+        print(contacts)
+
+        return render_template('view_contacts.html', contacts=contacts)
+
+    def add_contact(self):
+        if request.method == 'POST':
+            name = request.form['name']
+            phone = request.form['phone']
+            email = request.form['email']
+            contact_details = ContactDetail(name=name, email=email, phone=phone)
+            db.session.add(contact_details)
+            db.session.commit()
+            return redirect(url_for('view_contacts'))
+        else:
+            return render_template('add_contact.html', contact=None)
+
+    def update_contact(self, pk):
+        contact = ContactDetail.query.filter_by(id=pk).first()
+        if request.method == 'POST':
+            name = request.form['name']
+            phone = request.form['phone']
+            email = request.form['email']
+
+            contact.name = name
+            contact.phone = phone
+            contact.email = email
+            db.session.commit()
+            return redirect(url_for('view_contacts'))
+        else:
+            return render_template('add_contact.html', contact=contact)
+
+    def delete_contact(self, pk):
+        contact = ContactDetail.query.filter_by(id=pk).first()
+        db.session.delete(contact)
+        db.session.commit()
+        return redirect(url_for('view_contacts'))
+
+
+contacts_view = ContactsView()
+
+app.add_url_rule('/view_contacts', view_func=contacts_view.view_contacts)
+app.add_url_rule('/add_contact', view_func=contacts_view.add_contact, methods=['GET', 'POST'])
+app.add_url_rule('/update_contact/<pk>', view_func=contacts_view.update_contact, methods=['GET', 'POST'])
+app.add_url_rule('/delete_contact/<pk>', view_func=contacts_view.delete_contact)
 
 
 if __name__ == '__main__':
